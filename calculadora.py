ed1050c61a0e850a1fde336056205fa7f38bbd04
Ian Rodriguez <ianrodriguez0201@gmail.com>
2025-04-03 11:53:29 -0400

    primer commit calculadora

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+def calculate(op, num1, num2):
+    if op == "suma":
+        print(num1 + num2)
+    if op == "resta":
+        print(num1 - num2)
+    if op == "multiplicacion":
+        print(num1 * num2)
+    if op == "division":
+        print(num1 / num2)
+
+
+
+
+calculate("resta", 9, 7)
